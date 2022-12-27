8f9f96e6f15a0e8e1544e487c5899b059ae87576
Alex-mima <aleksander.mima@primus.com.al>
2022-12-27 15:35:09 +0100

    Final new observe Project

--- src/app/obeserve/obeserve.component.ts
+++ src/app/obeserve/obeserve.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-obeserve',
   templateUrl: './obeserve.component.html',
   styleUrls: ['./obeserve.component.css'],
 })
-export class ObserveComponent {}
+export class ObserveComponent implements OnInit {
+  id: number;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+    });
+  }
+}
