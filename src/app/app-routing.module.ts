ff71547b1cfd8dfd74257e7a6cf4e4e32d7b5a4b
Alex-mima <aleksander.mima@primus.com.al>
2022-12-27 15:44:48 +0100

    Final finaaal first project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { ObserveComponent } from './obeserve/obeserve.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'user/:id', component: ObserveComponent },
+  { path: 'user/:id', component: UserComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ObserveComponent } from './obeserve/obeserve.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent, ObserveComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, UserComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
