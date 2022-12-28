48ff7b5f74ff578993589ac430aa14061bab2731
Alex-mima <aleksander.mima@primus.com.al>
2022-12-28 11:09:38 +0100

    day 9

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent implements OnInit, OnDestroy {
+  private ObservbleSubsrciption: Subscription;
 
-  ngOnInit() {}
+  constructor() {}
+  ngOnInit() {
+    // this.ObservbleSubsrciption = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(
+            new Error('I dont know how to count higher than 3 brudda!')
+          );
+        }
+        count++;
+      }, 1000);
+    });
+    this.ObservbleSubsrciption = customObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error);
+      },
+      () => {
+        console.log('completion');
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.ObservbleSubsrciption.unsubscribe();
+  }
 }
