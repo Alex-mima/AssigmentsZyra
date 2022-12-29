255f3ed0d2826000591a6d84c937cea338fcd853
Alex-mima <aleksander.mima@primus.com.al>
2022-12-29 16:59:51 +0100

    assigment dita10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgForm } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BigUdemy';
+
+  onSubmit(form: NgForm) {
+    console.log(NgForm);
+  }
 }
